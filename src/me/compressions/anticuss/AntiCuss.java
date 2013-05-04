6469714404e4beb1d7b38a4c550b37bf3c54d115
Compressions <bukkitnotbucket@gmail.com>
2013-05-04 12:31:29 -0400

    First commit.

--- src/me/compressions/anticuss/AntiCuss.java
+++ src/me/compressions/anticuss/AntiCuss.java
@@ -8,7 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class AntiCuss extends JavaPlugin {
 
-	//testf
+	//testfg
 	public void onEnable() {
 		getLogger().info("AntiCuss enabled!");
 		getConfig().options().copyDefaults(true);
