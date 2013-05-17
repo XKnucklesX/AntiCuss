5d55fc469c0e385537f6f7b8b8c0491a4a097bd6
Compressions <bukkitnotbucket@gmail.com>
2013-05-17 17:11:26 -0400

    Added cost for cussing.

--- src/me/compressions/anticuss/AntiCuss.java
+++ src/me/compressions/anticuss/AntiCuss.java
@@ -1,15 +1,32 @@
 package me.compressions.anticuss;
 
+
 import me.compressions.anticuss.commands.AntiCussCommand;
 import me.compressions.anticuss.commands.Clear;
 import me.compressions.anticuss.commands.Mute;
 import me.compressions.anticuss.commands.Reload;
+import net.milkbowl.vault.economy.Economy;
 
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AntiCuss extends JavaPlugin {
 	
-	public boolean muted;
+public static Economy econ = null;
+	
+	private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+	
+	public boolean muted = false;
 	//test
 	//testf
 	public void onEnable() {
@@ -22,6 +39,12 @@ public class AntiCuss extends JavaPlugin {
 		getCommand("clearchat").setExecutor(new Clear(this));
 		getCommand("mutechat").setExecutor(new Mute(this));
 		getCommand("mutechat").setExecutor(new Clear(this));
+		
+		if (!setupEconomy() ) {
+            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+		}
 	}
 	
 	public void onDisable() {
--- src/me/compressions/anticuss/AntiCussListener.java
+++ src/me/compressions/anticuss/AntiCussListener.java
@@ -1,6 +1,8 @@
 package me.compressions.anticuss;
 
 
+import net.milkbowl.vault.economy.EconomyResponse;
+
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,18 +14,28 @@ public class AntiCussListener implements Listener {
 	public AntiCussListener(AntiCuss instance) {
 		plugin = instance;
 	}
-
+	
+	//TODO: charge players for cussing
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent e) {
 		for(String word : e.getMessage().toLowerCase().split(" ")) {
 			if(e.getPlayer().hasPermission("anticuss.bypass") || e.getPlayer().isOp()) {
 				e.setCancelled(false);
 			} else {
-
 				if(!e.getPlayer().hasPermission("anticuss.kick") && plugin.getConfig().getStringList("cusswords").contains(word)) {
 				e.setCancelled(true);
 				String warnmessage = plugin.getConfig().getString("warnmessage");
 				e.getPlayer().sendMessage(ChatColor.RED + warnmessage);
+				int cost = plugin.getConfig().getInt("cost");
+				if(cost == 0) {			
+				} else if(cost > 0) {
+					EconomyResponse cussr = AntiCuss.econ.depositPlayer(e.getPlayer().getName(), cost);
+					if(cussr.transactionSuccess()) {
+						e.getPlayer().sendMessage(ChatColor.RED + "You were charged " + cost + " for cussing!");
+					} else {
+						e.getPlayer().sendMessage(ChatColor.RED + "An error occurred!");
+					}
+				}
 			} else {
 				if(e.getPlayer().hasPermission("anticuss.kick") && plugin.getConfig().getStringList("cusswords").contains(word)) {
 					e.setCancelled(true);
